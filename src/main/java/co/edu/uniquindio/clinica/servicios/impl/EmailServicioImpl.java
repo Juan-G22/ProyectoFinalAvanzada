a5f6d36bdb930af2d5f437160def3e8ef369094f
Juan David Giraldo <juand.giraldob@uqvirtual.edu.co>
2023-11-01 01:19:33 -0500

    Se creo el servicio de email y su respectiva implementacion

---
+++ src/main/java/co/edu/uniquindio/clinica/servicios/impl/EmailServicioImpl.java
@@ -0,0 +1,30 @@
+package co.edu.uniquindio.clinica.servicios.impl;
+
+import co.edu.uniquindio.clinica.dto.EmailDTO;
+import co.edu.uniquindio.clinica.servicios.interfaces.EmailServicio;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+
+@Service
+@RequiredArgsConstructor
+public class EmailServicioImpl implements EmailServicio {
+
+
+    private final JavaMailSender javaMailSender;
+    @Override
+    public void enviarEmail(EmailDTO emailDTO) throws Exception {
+
+        MimeMessage mensaje = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mensaje);
+        helper.setSubject(emailDTO.asunto());
+        helper.setText(emailDTO.mensaje(), true);
+        helper.setTo(emailDTO.para());
+        helper.setFrom("no_reply@dominio.com");
+        javaMailSender.send(mensaje);
+
+    }
+}
---
+++ src/main/java/co/edu/uniquindio/clinica/servicios/interfaces/EmailServicio.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.clinica.servicios.interfaces;
+
+import co.edu.uniquindio.clinica.dto.EmailDTO;
+
+public interface EmailServicio {
+    void enviarEmail(EmailDTO emailDTO) throws Exception;
+}
